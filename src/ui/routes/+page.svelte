b6423cd108daa9551094abe0c0826e77a5fcf206
cyberowl <cybrowl@protonmail.com>
2024-11-18 23:53:04 -0800

    fix: import carta and setup the home page

--- src/ui/routes/+page.svelte
+++ src/ui/routes/+page.svelte
@@ -1,47 +1,16 @@
 <script>
-	import { createEventDispatcher, onMount } from 'svelte';
-	import { MainToolBar, Icon } from 'poro-components-ui';
-	import { Editor, rootCtx, defaultValueCtx, EditorStatus } from '@milkdown/core'; // Add rootCtx here
-	import { commonmark } from '@milkdown/kit/preset/commonmark';
-	import { nord } from '@milkdown/theme-nord';
+	import { Icon, MainToolBar } from 'poro-components-ui';
 
-	const dispatch = createEventDispatcher();
-	export let has_chats = false;
+	import { Carta, MarkdownEditor } from 'carta-md';
+	import '../carta_default.css';
 
-	let editor;
-	let editorContainer;
-
-	onMount(async () => {
-		editorContainer = document.querySelector('#editor_root');
-		if (editorContainer) {
-			try {
-				editor = Editor.make()
-					.config((ctx) => {
-						ctx.set(rootCtx, editorContainer); // Set the root explicitly
-						ctx.set(defaultValueCtx, 'Ask Me Anything!');
-					})
-					.use(commonmark)
-					.onStatusChange((status) => {
-						if (status === EditorStatus.Created) {
-							console.log('Milkdown editor fully created');
-						} else if (status === EditorStatus.OnCreate) {
-							console.log('Milkdown editor initializing...');
-						} else if (status === EditorStatus.OnDestroy) {
-							console.log('Milkdown editor destroying...');
-						} else if (status === EditorStatus.Destroyed) {
-							console.log('Milkdown editor destroyed');
-						}
-					});
-
-				await editor.create();
-				console.log('Editor container content after creation:', editorContainer.innerHTML);
-			} catch (error) {
-				console.error('Error initializing Milkdown editor:', error);
-			}
-		} else {
-			console.error('Editor container not found.');
-		}
+	const carta = new Carta({
+		theme: 'dracula'
 	});
+
+	let value = '';
+
+	export let has_chats = true;
 </script>
 
 <div class="grid_layout">
@@ -63,7 +32,11 @@
 		<div class="toolbar">
 			<MainToolBar />
 		</div>
-		<div class="editor_root" id="editor_root"></div>
+		<div class="editor_root">
+			<div class="markdown">
+				<MarkdownEditor bind:value mode="split" {carta} disableToolbar={true} placeholder="#" />
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -73,6 +46,11 @@
 </div>
 
 <style lang="postcss">
+	:global(.carta-font-code) {
+		font-family: '...', monospace;
+		font-size: 1.1rem;
+	}
+
 	.grid_layout {
 		@apply hidden lg:grid grid-cols-12 gap-y-2 place-items-center h-screen fixed inset-0;
 	}
@@ -80,12 +58,15 @@
 		@apply col-start-1 col-end-7 row-start-1 row-end-auto flex justify-center items-center bg-black-a w-full h-full;
 	}
 	.write_main_layout {
-		@apply col-start-7 col-end-13 row-start-1 row-end-auto flex flex-col bg-black-b w-full h-full;
+		@apply col-start-7 col-end-13 row-start-1 row-end-auto justify-between flex flex-col bg-black-b w-full h-full;
 	}
 	.toolbar {
-		@apply fixed top-8 right-0 w-1/4 h-16 bg-black-b z-10;
+		@apply w-full h-16 bg-black-b z-10 flex items-center justify-end p-10;
 	}
 	.editor_root {
-		@apply mt-40 w-full h-full overflow-auto bg-black-b p-4 text-white;
+		@apply flex-grow p-4;
+	}
+	.markdown {
+		@apply overflow-auto w-full h-full fixed mb-10 z-10;
 	}
 </style>
